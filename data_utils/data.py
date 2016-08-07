a07ae1d5c3ce7e950d7ec92369b31f8f60e3944d
xenx <atvorozhkov@yandex.ru>
2016-08-08 02:01:38 +0300

    Add resize for train data

--- data_utils/data.py
+++ data_utils/data.py
@@ -1,4 +1,4 @@
-from dill import dump, load
+import dill
 from numpy.random import choice
 from librosa import load, logamplitude
 from librosa.feature import melspectrogram
@@ -18,17 +18,17 @@ class Users():
         # Путь до dill модели
         self.path = 'users.dl'
         # Defaultdict информации
-        self.base = load(open(self.path, 'rb'))
+        self.base = dill.load(open(self.path, 'rb'))
 
     def __getitem__(self, item):
         return self.base[item]
 
     def save(self):
         ''' Поскольку это не стандартная DB тут нужна функция сохранения. '''
-        dump(self.base, open(self.path, 'wb'))
+        dill.dump(self.base, open(self.path, 'wb'))
 
     def get_train(self):
-        '''Делаем из данных train выборку'''
+        ''' Делаем из данных train выборку '''
         train = []
         for key in self.base.keys():
             if len(self.base[key]) >= 2:
@@ -45,14 +45,24 @@ class Users():
         '''Сохраняем спектограму для пользователя'''
         self.base[user][word] = get_spectgorgamm('/tmp/file.wav')
 
-    def get_train_for_user(self, user):
+    def get_train_for_user(self, user, shape = (100,20)):
+        ''' Трейн данные для одного пользователя '''
         train = []
         if len(self.base[user]) >= 2:
             for _ in range(0, len(self.base[user]), 2):
                 values = choice(list(self.base[user]), 2)
+                
+                value_first = np.asarray(self.base[user][values[0]])
+                value_second = np.asarray(self.base[user][values[1]]
+                
                 other = choice(list(self.base), 1)[0]
-                other_value = choice(list(self.base[other]), 1)[0]
-                train.append((user, self.base[user][values[0]],
-                              other, self.base[other][other_value], user, self.base[user][values[1]]))
+                other_value = np.asarray(self.base[other][choice(list(self.base[other]), 1)[0]])
+                
+                np.resize(other_value, shape)
+                np.resize(value_first, shape)
+                np.resize(value_second, shape)
+                                          
+                train.append((user, value_first,
+                              other, other_value, user, value_second))
         else:
             raise ValueError
