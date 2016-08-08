e815089a8b953afadb1f013c5cb234ab63fe2155
xenx <atvorozhkov@yandex.ru>
2016-08-08 05:45:21 +0300

    добавлена отрисовка

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
 from neural.model import NeuralNetwork
 from data_utils.data import Users
+from flask import Flask, render_template
 
 # Загрузим модель
 our_network = NeuralNetwork()
 
 # Загрузим данные
-train_data = Users().get_train()
+data = Users()
 
 # Обучимся
-our_network.train(train_data)
+our_network.train(data.get_train())
+
+vectors_x = []
+vectors_y = []
+names = []
+labels = []
+for user in data.base.keys():
+    names.append(user)
+    x, y = [], []
+    label = []
+    for word in data.base[user].keys():
+        label.append(word)
+        voice = data[user][word]
+        a = voice.copy()
+        a.resize(100, 20)
+
+        predicted = our_network.predict([[a]])[0][0]
+
+        x.append(predicted[0])
+        y.append(predicted[1])
+
+    labels.append(label)
+
+    vectors_x.append(x)
+    vectors_y.append(y)
+
+new_data = '['
+
+for i in range(len(names)):
+    new_data += 'l' + str(i) +','
+
+new_data += ']'
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template('index.html', vectors_x=vectors_x,
+                           vectors_y=vectors_y, names=names, labels=labels,
+                           data=new_data)
+
+
+if __name__ == "__main__":
+    app.run()
--- neural/model.py
+++ neural/model.py
@@ -4,7 +4,7 @@ from lasagne.layers import InputLayer, DenseLayer, \
     ReshapeLayer, Conv1DLayer, MaxPool1DLayer, \
     GlobalPoolLayer, get_output, get_all_params
 
-from lasagne.nonlinearities import very_leaky_rectify, sigmoid
+from lasagne.nonlinearities import very_leaky_rectify, tanh
 
 from lasagne.updates import adagrad
 
@@ -13,7 +13,7 @@ import theano
 
 class NeuralNetwork:
     def __init__(self):
-        num_units = 150  # выходной вектор будет в этом пространстве
+        num_units = 2  # выходной вектор будет в этом пространстве
 
         # Входной вектор (Кол-во батчей, кол-во записей, время, частота)
         input_X = T.tensor4("X")
@@ -24,7 +24,7 @@ class NeuralNetwork:
         # Сворачиваем все записи друг за другом
         input_reshape = ReshapeLayer(input_layer, (-1, 20, 100))
 
-        convolution = Conv1DLayer(input_reshape, num_filters=200, filter_size=5,
+        convolution = Conv1DLayer(input_reshape, num_filters=100, filter_size=5,
                                   nonlinearity=very_leaky_rectify)
 
         pooling = MaxPool1DLayer(convolution, 2)
@@ -33,7 +33,7 @@ class NeuralNetwork:
 
         dense = DenseLayer(global_pooling, num_units=300)
 
-        output_dense = DenseLayer(dense, num_units=num_units, nonlinearity=sigmoid,
+        output_dense = DenseLayer(dense, num_units=num_units, nonlinearity=tanh,
                                   name='output')
 
         train_output = ReshapeLayer(output_dense, (-1, 3, num_units))
